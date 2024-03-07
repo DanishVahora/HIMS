981a253b2438005d592af1b7056569569440e435
danish <danishvhora786@gmail.com>
2024-03-07 14:56:44 +0530

    Model Updated

--- new/views.py
+++ new/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'login.html')
+
+def aboutme(request):
+    return render(request,'login.html')
